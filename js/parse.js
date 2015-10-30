58ec6dc144629af4c9cb78f8814d43711ea7a0b3
Ying Dang <whydang@uw.edu>
2015-10-30 12:11:05 -0700

    readjusted fonts and whatnot, ready to implement parse

--- js/parse.js
+++ js/parse.js
@@ -5,12 +5,11 @@ $(document).ready(function() {
 	$('img').fadeTo(2000, 1);
 });
 
-$('#test')[0].onload = function() {
-	alert("");
-};
-
 // Initialize Parse app
-Parse.initialize("bj0XEgiqJuXgawhlDKdvgZkExjfKTRYwEUIzqroG", "ypMTQVrr0vW2y3MRueaRu6RDjyh7cKDx7Bgch1Aq");
+ Parse.initialize("bj0XEgiqJuXgawhlDKdvgZkExjfKTRYwEUIzqroG", "ypMTQVrr0vW2y3MRueaRu6RDjyh7cKDx7Bgch1Aq");
 
 // Create a new sub-class of the Parse.Object, with name "Reviews"
-var Reviews = Parse.Object.extend('Reviews');
+ var Reviews = Parse.Object.extend('Reviews');
+
+// Create a new instance of that class.
+var reviews = new Reviews();
