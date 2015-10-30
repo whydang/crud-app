7a46ab167e02d745a4bf221de2e8fc9659272425
Ying Dang <whydang@uw.edu>
2015-10-30 01:46:21 -0700

    added faded feature

--- js/parse.js
+++ js/parse.js
@@ -1,4 +1,13 @@
+
+
 // this is the js file to store and make user interactions
+$(document).ready(function() {
+	$('img').fadeTo(2000, 1);
+});
+
+$('#test')[0].onload = function() {
+	alert("");
+};
 
 // Initialize Parse app
 Parse.initialize("bj0XEgiqJuXgawhlDKdvgZkExjfKTRYwEUIzqroG", "ypMTQVrr0vW2y3MRueaRu6RDjyh7cKDx7Bgch1Aq");
