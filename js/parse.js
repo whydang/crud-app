0046fbf4633dc7aea5e7c1bfd5110d9662ab8fe8
Ying Dang <whydang@uw.edu>
2015-10-31 02:23:49 -0700

    fixing parse

--- js/parse.js
+++ js/parse.js
@@ -1,5 +1,6 @@
 
-
+// track star input user made
+var currentStar = -1;
 
 
 // this is the js file to store and make user interactions
@@ -8,9 +9,37 @@ $(document).ready(function() {
 
 	var starArray = $('.star');
 
+	// fills in any stars that are hovered over to represent the rating
 	$('.star').hover(function() {
 		
-		var starID = $(this).attr('id');
+		var index = findIndex(this);
+
+		toggleStars(true, 0, index);
+		toggleStars(false, index, starArray.length);
+
+	});
+
+	// unselects all stars if the div is not hovered
+	$("#star-rating").hover(function() {}, function() {
+		if(currentStar == -1) {
+			toggleStars(false, 0, starArray.length);
+		} else {
+			toggleStars(false, 0, starArray.length);
+			toggleStars(true, 0, currentStar);
+		}
+	});
+
+	// toggle the star to either filled or not filled, depending on arguments
+	function toggleStars(state, start, end) {
+		for (var i = start; i < end; i++) {
+			$(starArray[i]).toggleClass("fa-star-o", !state);
+			$(starArray[i]).toggleClass("fa-star", state);
+		}
+	}
+
+	// finds the index of the given star identity
+	function findIndex(given) {
+		var starID = $(given).attr('id');
 
 		var index;
 		switch (starID) {
@@ -32,31 +61,20 @@ $(document).ready(function() {
 			default:
 				index = -1;
 		}
-
-		toggleStars(true, 0, index);
-		toggleStars(false, index, starArray.length);
-	});
-
-	$("#star-rating").hover(function() {}, function() {
-		toggleStars(false, 0, starArray.length);
-	});
-
-	function toggleStars(state, start, end) {
-		for (var i = start; i < end; i++) {
-			$(starArray[i]).toggleClass("fa-star-o", !state);
-			$(starArray[i]).toggleClass("fa-star", state);
-		}
+		return index;
 	}
+
+	// colors over the given star identity
+	$('.star').click(function() {
+		var index = findIndex(this);
+		toggleStars(true, 0, index);
+		currentStar = index;
+		console.log(currentStar);
+
+	});
+	
 });
 
-
-// var handleIn = function(star) {
-	
-// };
-
-// var handleOut = fuction() {
-
-// };
 
 // Initialize Parse app
  Parse.initialize("bj0XEgiqJuXgawhlDKdvgZkExjfKTRYwEUIzqroG", "ypMTQVrr0vW2y3MRueaRu6RDjyh7cKDx7Bgch1Aq");
@@ -73,5 +91,37 @@ $('form').submit(function() {
 		$(this).val('');
 	});
 
+	reviews.set('rate', currentStar);
+	currentStar = -1;
+
+	// After setting each property, save your new instance back to your database
+	reviews.save(null, {
+		success:function() {
+			getData();
+		}
+	}, {
+		error: function() {
+			alert('unable to save');
+		}
+	});
+	return false;
+
 });
+
+function getData() {
+	// Set up a new query for our Music class
+	var query = new Parse.Query(Reviews);
+
+	// Set a parameter for your query -- where the website property isn't missing
+	//query.notEqualTo('website', '')
+
+	/* Execute the query using ".find".  When successful:
+	    - Pass the returned data into your buildList function
+	*/
+	query.find({
+		success:function(results) {
+			//buildList(results);
+		}
+	});
+};
 
