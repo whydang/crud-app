c2cff393fba625cd72a85ff524a8dfbfe13cc8c1
Ying Dang <whydang@uw.edu>
2015-10-31 07:51:02 -0700

    complete

--- js/parse.js
+++ js/parse.js
@@ -1,16 +1,17 @@
 
 // track star input user made
-var currentStar = -1;
-
+var currentStar = 0;
+var totalStar = 0;
+var reviewCount = 0;
 
 // this is the js file to store and make user interactions
 $(document).ready(function() {
 	$('img').fadeTo(2000, 1);
 
-	var starArray = $('.star');
+	var starArray = $('#star-rating .star');
 
 	// fills in any stars that are hovered over to represent the rating
-	$('.star').hover(function() {
+	$('#star-rating .star').hover(function() {
 		
 		var index = findIndex(this);
 
@@ -21,7 +22,7 @@ $(document).ready(function() {
 
 	// unselects all stars if the div is not hovered
 	$("#star-rating").hover(function() {}, function() {
-		if(currentStar == -1) {
+		if(currentStar == 0) {
 			toggleStars(false, 0, starArray.length);
 		} else {
 			toggleStars(false, 0, starArray.length);
@@ -65,63 +66,152 @@ $(document).ready(function() {
 	}
 
 	// colors over the given star identity
-	$('.star').click(function() {
+	$('#star-rating .star').click(function() {
 		var index = findIndex(this);
 		toggleStars(true, 0, index);
 		currentStar = index;
-		console.log(currentStar);
-
 	});
 	
+	// Initialize Parse app
+	Parse.initialize("bj0XEgiqJuXgawhlDKdvgZkExjfKTRYwEUIzqroG", "ypMTQVrr0vW2y3MRueaRu6RDjyh7cKDx7Bgch1Aq");
+
+	// Create a new sub-class of the Parse.Object, with name "Reviews"
+	var Reviews = Parse.Object.extend('Reviews');
+
+	$('form').submit(function() {
+		// Create a new instance of that class.
+		var reviews = new Reviews();
+
+		$(this).find('input').each(function() {
+			reviews.set($(this).attr('id'), $(this).val());
+			$(this).val('');
+		});
+
+		reviews.set('rate', currentStar);
+		currentStar = 0;
+
+		reviews.set('up', 0);
+		reviews.set('down', 0);
+
+		// After setting each property, save your new instance back to your database
+		reviews.save(null, {
+			success:function() {
+				getData();
+			}
+		}, {
+			error: function() {
+				alert('unable to save');
+			}
+		});
+		return false;
+	});
+
+	// this function gets the data and processes them
+	function getData() {
+		// Set up a new query for our Music class
+		var query = new Parse.Query(Reviews);
+		query.notEqualTo('rate', 0);
+
+		/* Execute the query using ".find".  When successful:
+		    - Pass the returned data into your buildList function
+		*/
+		query.find({
+			success:function(results) {
+				buildList(results);
+			}
+		});
+	};
+
+	// populates the list with reviews
+	function buildList(results) {
+		reviewCount = 0;
+		totalStar = 0;
+		results.forEach(function(data) {
+			addItem(data);
+		});
+	}
+
+	var star_half = "<i class='fa fa-star-half-o star'></i>";
+	var star_filled = "<i class='fa fa-star star'></i>";
+	var star = "<i class='fa fa-star-o star'></i>";
+	var like = "<i class='fa fa-thumbs-o-up'></i>";
+	var dislike = "<i class='fa fa-thumbs-o-down'></i>";
+
+	function addItem(data) {
+		var title = data.get('title');
+		var desc = data.get('desc');
+		var rating = data.get('rate');
+
+		reviewCount++;
+		totalStar += rating;
+
+		var box = $("<div class='form-group review-group'></div>");
+		var titleEle = $("<h3>" + title + "</h3>");
+		var descEle = $('<p>"' + desc + '"</p>');
+
+		// Two for loops, one for filled stars
+		// One for not filled stars
+		// 0 - rating, rating - max rating
+		var starString = "";
+		for(var i = 0; i < rating; i++) {
+			starString += star_filled;
+		}
+		for(var i = rating; i < 5; i++) {
+			starString += star;
+		}
+
+		var starEle = $("<div class='review-star'>" + starString + "</div>");
+
+		box.append(starEle);
+		box.append(titleEle);
+		box.append(descEle);
+
+		box.append($("<div class='move uplike'>" + like + "</div>"));
+		box.append($("<div class='move downlike'>" + dislike + "</div>"));
+		
+
+
+		box.append($("<p>" + data.get('up') + " out of " + Number(data.get('up') + data.get('down')) + " found this helpful</p>"));
+		$('.review-area').append(box);
+		computeAverage();
+	}
+
+	// computes the average of the stars of all reviews
+	function computeAverage() {
+		var averageStar = Number(totalStar / reviewCount);
+		var averageRate = "";
+		var stop = 5;
+
+		for (var i = 1; i <= averageStar; i++) {
+			averageRate += star_filled;
+		}
+
+		if (Math.floor(averageStar) != averageStar) {
+			averageRate += star_half;
+			stop--;
+		}
+
+		for (var i = averageStar; i < stop; i ++) {
+			averageRate += star;
+		}
+
+		$('#avg-rating i').remove();
+		$('#avg-rating').append(averageRate);
+	}
+
+	//increment thumb up on click
+	$('.uplike').click(function() {
+		gameScore.increment("up");
+		gameScore.save();
+	});
+
+	//increment thumb down on click
+	$('.downlike').click(function() {
+		gameScore.increment("down");
+		gameScore.save();
+	});
+
+	getData();
 });
 
-
-// Initialize Parse app
- Parse.initialize("bj0XEgiqJuXgawhlDKdvgZkExjfKTRYwEUIzqroG", "ypMTQVrr0vW2y3MRueaRu6RDjyh7cKDx7Bgch1Aq");
-
-// Create a new sub-class of the Parse.Object, with name "Reviews"
- var Reviews = Parse.Object.extend('Reviews');
-
-$('form').submit(function() {
-
-	// Create a new instance of that class.
-	var reviews = new Reviews();
-	$(this).find('input').each(function() {
-		reviews.set($(this).attr('id'), $(this).val());
-		$(this).val('');
-	});
-
-	reviews.set('rate', currentStar);
-	currentStar = -1;
-
-	// After setting each property, save your new instance back to your database
-	reviews.save(null, {
-		success:function() {
-			getData();
-		}
-	}, {
-		error: function() {
-			alert('unable to save');
-		}
-	});
-	return false;
-
-});
-
-function getData() {
-	// Set up a new query for our Music class
-	var query = new Parse.Query(Reviews);
-
-	// Set a parameter for your query -- where the website property isn't missing
-	//query.notEqualTo('website', '')
-
-	/* Execute the query using ".find".  When successful:
-	    - Pass the returned data into your buildList function
-	*/
-	query.find({
-		success:function(results) {
-			//buildList(results);
-		}
-	});
-};
 
